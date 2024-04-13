4c4e0958f6b10f7ae5e83990b8d09445bffac86d
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-04-13 07:03:05 +0200

    Move app.py to app dir

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import yaml
 
 app = Flask(__name__)
@@ -9,3 +9,8 @@ def index():
         config = yaml.safe_load(f)
     posts = config["posts"]
     return render_template("index.html", posts=posts)
+
+
+@app.route("/admin/posts", methods=["POST"])
+def create_post():
+    pass
